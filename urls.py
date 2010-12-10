da615e8a2831e1bec8512eb5475c8af16ac4745c
Len Taing <lentaing@jimmy.harvard.edu>
2010-12-10 23:49:00 +0000

    SUB_SITE Re-Factoring:
    PROBLEM: django likes to be the root url, it is very when you want to
    deploy it under a prefix/sub-site e.g. cistrome.dfci.harvard.edu/newdc/
    
    SOLN:
    ref: http://opensourcemissions.wordpress.com/2010/03/12/finally-a-working-django-non-root-url-with-mod_wsgi/
    
    I followed this guy's howto, and hopefully, this project is much more flexible!
    
    NOTED CHANGES:
    1. using url to print urls instead of hard coding them!
    2. static/ urls, are prepended by {% url static_url %}

--- urls.py
+++ urls.py
@@ -1,35 +1,7 @@
+# urls.py
 from django.conf.urls.defaults import *
-#import datacollection as datacollection
-from datacollection import views
-from django.contrib.auth.views import login, logout
 import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-PREFIX_URL = settings.PREFIX_URL.lstrip("/")
-
 urlpatterns = patterns('',
-              (r'^'+PREFIX_URL+'$', views.papers),
-              (r'^'+PREFIX_URL+'new_paper_form/$', views.new_paper_form),
-              (r'^'+PREFIX_URL+'new_dataset_form/$', views.new_dataset_form),
-              (r'^'+PREFIX_URL+'all_papers/$', views.all_papers),
-              (r'^'+PREFIX_URL+'get_datasets/(\d+)/$', views.get_datasets),
-              (r'^'+PREFIX_URL+'datasets/$', views.datasets),
-              (r'^'+PREFIX_URL+'accounts/login/$', login),
-              (r'^'+PREFIX_URL+'accounts/logout/$', logout),
-              #(r'^'+PREFIX_URL+'accounts/register/$', views.register),
-              (r'^'+PREFIX_URL+'static/(?P<path>.*)$', 
-               'django.views.static.serve',
-               {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    # Example:
-    # (r'^newdc/', include('newdc.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^%s' % settings.SUB_SITE, include('urls_subsite')),
 )
