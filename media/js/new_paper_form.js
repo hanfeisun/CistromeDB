8482ba9b51a64474fbaa6409b057a0f047f90a16
Len Taing <lentaing@jimmy.harvard.edu>
2011-02-15 20:58:56 +0000

    Revised the new_paper_form to only include the fields that are mocked-up in the student wireframes--i.e. minimal fields Modified papers.html to link to the paper_profiles. also added a link to the new_paper_form page Modified base.html to include login_form block

--- media/js/new_paper_form.js
+++ media/js/new_paper_form.js
@@ -1,3 +1,30 @@
+function init() {
+    //create the journal choices
+    var cb = function(req) {
+	var journals = eval("("+req.responseText+")");
+
+	var container = $('journal');
+	container.appendChild($D('label', {'for':'journal', 
+					 'innerHTML':'Journal'}));
+	
+	var select = $D('select', {'name':'journal', 'id':'journal'})
+	select.appendChild($D('option',{'value':'','innerHTML':'----------'}));
+	for (var i = 0; i < journals.length; i++) {
+	    select.appendChild($D('option',{'value':journals[i].id,
+					  'innerHTML':journals[i].name}));
+	}
+	container.appendChild(select);
+
+	var a = $D('a', {'href':SUB_SITE+"new_journal_form/",
+			   'innerHTML':"add new journal"});
+	container.appendChild(a);
+    }
+    var getJournals = 
+	new Ajax.Request(SUB_SITE+"jsrecord/Journals/all/", 
+			 {method:"get", onSuccess: cb});
+}
+
+/*
 var containers;
 
 function init() {
@@ -20,3 +47,4 @@ function init() {
     }
 }
 
+*/
