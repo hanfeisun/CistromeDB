6fd34d81f27f1dad17ee6f0e40ed96f92e62d26b
Len Taing <lentaing@jimmy.harvard.edu>
2011-03-15 00:22:43 +0000

    Major rewrite of xls_importer: 1. uses openpyxl which is a package that reads xls files 2. imports through django rather than through web service calls

--- importer/xls_importer.py
+++ importer/xls_importer.py
@@ -1,66 +1,114 @@
+#!/usr/bin/env python
 """
-Synopsis: given a tab-delimited representation of the tongji xls file,
-this script tries to import all of the papers into the newdc db
+Synopsis: Given a xls(x) file filled with dc dataset information, try to
+import the data into the newdc using autoimport fns
+
+NOTE: this is a major rewrite of the original xls importer b/c now we
+1. rely on the openpyxl package to read in xls files, and
+2. we are not going through the web to create the datasets, but will
+do it all in django
+"""
+import os
+import sys
+import optparse
+import traceback
+
+import importer_settings
+sys.path.insert(0,importer_settings.DEPLOY_DIR)
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+from datacollection import models
+from datacollection import views
+from django.contrib.auth.models import User
+from entrezutils import models as entrez
+
+from openpyxl.reader.excel import load_workbook
+
+USAGE = """USAGE: xls_importer.py -f foo.xlsx
+Parameters:
+         -f - the xls file to import
 """
 
-import sys
-import time
-import re
-import httplib
-import urllib, urllib2
 
 def main():
-    txt_filename = "stat.txt"
-    max_papers = 50 #set a max number of papers to import, or set to -1 for all
-    gse_pattern = re.compile(r"GSE\d{5}")
+    parser = optparse.OptionParser(usage=USAGE)
+    parser.add_option("-f", "--file",
+                      help="path to the dir which to search for packages")
+    (opts, args) = parser.parse_args(sys.argv)
+    if not opts.file:
+        print USAGE
+        sys.exit(-1)
 
-    #grab GSEIDs from the file
-    gseids = []
-    
-    f = open(txt_filename)
+    if not os.path.exists(opts.file):
+        print "File does not exist"
+        sys.exit(-1)
 
-    line = f.readline()
-    f.close()
-    #NOTE: i can't get dos2unix to work; it's read as one big line...blame MSFT
-    tokens = line.split() #each thing will be a token
-    for t in tokens:
-        if gse_pattern.match(t) and not (t in gseids):
-            gseids.append(t)
+    #try to load the workbook:
+    wb = load_workbook(opts.file)
+    sheet = wb.get_active_sheet()
 
-    if max_papers >= 0:
-        m = max_papers
-    else:
-        m = len(gseids)
+    #load the data into a list of dictionaries
+    cols = []
+    entries = []
+    for (i,r) in enumerate(sheet.rows):
+        if i == 0:
+            #the first row defines the columns
+            cols = [c.value for c in r]
+        else:
+            tmp = {}
+            for (key, cell) in zip(cols, r):
+                tmp.__setitem__(key, cell.value)
+            entries.append(tmp)
 
-    #LOGIN- ref: http://personalpages.tds.net/~kent37/kk/00010.html
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-    urllib2.install_opener(opener)
-    params = urllib.urlencode(dict(username='lentaing', password='lentaing'))
-    f = opener.open('http://localhost:8000/accounts/login/', params)
-    data = f.read()
-    f.close()
+    #maps xls student names to dc usernames
+        #users = User.objects.all()
+    userDict = {"Xueqiu Lin":"xueqiulin",
+                "Ying Ge": "yingge",
+                "Hanfei Sun": "hanfeisun",
+                "Su Wang": "suwang",
+                "Luying Wang": "luyingwang",
+                "Mingyang Wang": "mingyangwang",
+                "Chenyang Wang": "chenyangwang",
+                "Xikun Duan": "xinkunduan",
+                "Hongtao Sun": "hongtaosun",
+                "Qixuan Wang": "qixuanwang",
+                "Bo Qin": "boqin",
+                "Haiyang Zheng": "haiyangzheng",
+                }
 
-    #now feed these into the autoimporter
-    papers_added = []
-    for i in range(m):
+    gsm_pattern = "^GSM\d{6}$"
+    #another pass through the data
+    not_processed = []
+    for (i, e) in enumerate(entries):
+        #try to get the paper associated with the dataset
         try:
-            params = urllib.urlencode(dict(gseid=gseids[i], json="true"))
-            f = urllib2.urlopen('http://localhost:8000/auto_paper_import/',
-                                params)
-            data = f.read()
-            f.close()
-            if data.find("true") > 0:
-                papers_added.append(gseids[i])
-            #wait 1 second between posts
-            time.sleep(1)
+            p = models.Papers.objects.filter(gseid=e['GSEID'])
+            if not p:
+                #need to create a new paper
+                username = userDict[e['Student Name']]
+                if username:
+                    user = User.objects.filter(username=username)[0]
+                else:
+                    #default to lentaing/shirleyliu
+                    user = User.objects.get(pk=1)
+                #create the paper
+                p = views._import_paper(e['GSEID'], user)
+                #import the datasets
+                geoQuery = entrez.PaperAdapter(p.gseid)
+                views._auto_dataset_import(p, user, geoQuery.datasets)
         except:
-            print sys.exc_info()[0]
+            print "Exception in user code:"
+            print '-'*60
+            traceback.print_exc(file=sys.stdout)
+            print '-'*60
+            not_processed.append(i)
+            #sys.exit(-1)
 
-    f = open("papers_added.txt","w")
-    for p  in papers_added:
-        f.write("%s\n" % p)
+    f = open("not_processed.txt", "w")
+    for n in not_processed:
+        f.write(n)
     f.close()
-            
-
+    
 if __name__ == "__main__":
     main()
