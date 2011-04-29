2adc6fb08b28cfdba9060269bfe95802f34d1320
Len Taing <lentaing@jimmy.harvard.edu>
2011-04-29 02:39:41 +0000

    adding sub_site template tag to help w/ the sub_site problem for now, i just fixed the sub_site link problems in sample_profile.

--- datacollection/templatetags/dc_extras.py
+++ datacollection/templatetags/dc_extras.py
@@ -1,9 +1,16 @@
 from django.template.defaultfilters import stringfilter
 from django import template
 
+import settings
+
 register = template.Library()
 
 @register.filter
 @stringfilter
 def split(value, arg):
     return value.split(arg)
+
+@register.filter
+@stringfilter
+def sub_site(value):
+    return settings.SUB_SITE + value
