e80be81ec57fc85aab8423465f02e7aa183fbe15
Len Taing <lentaing@jimmy.harvard.edu>
2011-05-27 14:16:31 -0400

    Pipeline part 3: full mock run implemented! Now on click run: 1. the conf file is generated 2. a runner script is generated 3. the runner script is run w/ a python daemonizer 4. on complete, the package is imported 5. email notification is sent to user
    **NOW to test w/ actual tools! (that's part 4)

--- pipeline/RunSHGenerator.py
+++ pipeline/RunSHGenerator.py
@@ -8,6 +8,26 @@ import settings
 #Assumes that pipeline path is the parent dir of settings.CONF_TEMPLATE_DIR
 _PIPELINE_PATH = "/".join(settings.CONF_TEMPLATE_DIR.split("/")[:-1])
 _ChipSeqPipeline_PATH = os.path.join(_PIPELINE_PATH, "ChIPSeqPipeline.py")
+_Importer_PATH = os.path.join(settings.DEPLOY_DIR, "importer", 
+                              "datafiles_importer.py")
+
+def generateDaemonizer(dir="."):
+    """generates the python script that will daemonize the call to run script
+    """
+    py_template = """#!/usr/bin/python
+
+import daemon
+import subprocess
+
+with daemon.DaemonContext(working_directory="."):
+    proc = subprocess.Popen(["nohup", "bash", "run.sh"])
+"""
+    py_sh = open(os.path.join(dir, "daemonize.py"), "w")
+    py_sh.write(py_template)
+    
+    py_sh.close()
+    return py_sh
+    
 
 def generate(sample, conf_file, user, dir="."):
     """tries to generate a file called run.sh:
@@ -28,13 +48,26 @@ def generate(sample, conf_file, user, dir="."):
 #1. run the pipeline
 python %s %s
 
-#2. on completion, email notify the user
+#2. TEST for success--if the tar.gz is generated
+if [ -e %s.tar.gz ]
+then
+#TRY import
+%s -d %s
+
+fi 
+
+#3. on completion, email notify the user
 echo '%s' | sendmail -t
 """
     
     run_sh = open(os.path.join(dir, "run.sh"), "w")
     run_sh.write(script_template % (_ChipSeqPipeline_PATH, conf_file.name, 
+                                    "sample%s"%sample.id, _Importer_PATH, dir,
                                     email))
 
     run_sh.close()
+    
+    #generate the daemonize wrapper
+    py_sh = generateDaemonizer(dir)
+
     return run_sh
