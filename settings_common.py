35c18c75adf9c0e94e6f63f36957acb37752fbe4
Len Taing <lentaing@jimmy.harvard.edu>
2011-06-21 14:01:13 -0400

    Fixing a path not found bug:
    THE following caused errors on new daisy's deployed site:
    HAYSTACK_SITECONF = 'newdc.search_sites'
    
    This works for everyone:
    HAYSTACK_SITECONF = 'search_sites'

--- settings_common.py
+++ settings_common.py
@@ -92,6 +92,6 @@ AUTH_PROFILE_MODULE = 'datacollection.UserProfiles'
 CACHE_BACKEND = 'db://newdc_cache'
 
 #haystack specific
-HAYSTACK_SITECONF = 'newdc.search_sites'
+HAYSTACK_SITECONF = 'search_sites'
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
 HAYSTACK_WHOOSH_PATH = os.path.join(MEDIA_ROOT, 'whoosh_index')
