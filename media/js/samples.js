cc50d73028e772f8ac4a604154a29bf097a2968a
Len Taing <lentaing@jimmy.harvard.edu>
2011-07-28 13:48:18 -0400

    Samples/Datasets Name Swap: Part 5- media/css, media/js (untested commit)

--- media/js/samples.js
+++ media/js/samples.js
@@ -23,9 +23,9 @@ function model() {
 
 function clickHandler(checkbox, sampleId) {
     if (checkbox.checked) {
-	sampleModel.add(sampleId);
+	dsetModel.add(sampleId);
     } else {
-	sampleModel.remove(sampleId);
+	dsetModel.remove(sampleId);
     }
 }
 
@@ -37,10 +37,10 @@ function masterHandler(masterChkBox) {
 }
 
 var rowSelects;
-var sampleModel;
+var dsetModel;
 
 function init() {
-    sampleModel = new model();
+    dsetModel = new model();
     rowSelects = $$('input.rowSelect');
     for (var i = 0; i < rowSelects.length; i++) {
 	//rowSelects[i].checked=false; --maybe i don't want this
@@ -59,8 +59,8 @@ function init() {
     var btn = $('batchEdit');
     btn.onclick = function(event) {
 	//redirect
-	if (sampleModel.samples.length > 0) { //check for empty list
-	    window.location = SUB_SITE + "batch_update_samples/?samples="+ sampleModel.samples+"&next="+window.location;
+	if (dsetModel.samples.length > 0) { //check for empty list
+	    window.location = SUB_SITE + "batch_update_samples/?samples="+ dsetModel.samples+"&next="+window.location;
 	}
 	//NOTE: TODO--acutally the best way to do it is to just run down the 
 	//list of selected rows--i.e. generate dsetModel IN/AFTER onclick
@@ -71,17 +71,13 @@ function init() {
     var deleteBtn = $('deleteBtn');
     deleteBtn.onclick = function(event) {
 	//confirm
-	var resp = confirm("Are you sure you want to DELETE these papers?");
-	if (resp && sampleModel.samples.length > 0) {
-	    window.location = SUB_SITE+"delete_samples/?samples="+sampleModel.samples;
+	var resp = confirm("Are you sure you want to DELETE these samples?");
+	if (resp && dsetModel.samples.length > 0) {
+	    window.location = SUB_SITE+"delete_samples/?samples="+dsetModel.samples;
 	}
     }
 }
 
-function checkRawFiles(sampleId, pg) {
-    window.location = SUB_SITE+"check_raw_files/"+sampleId+"/?page="+pg;
-}
-
-function runAnalysis(sampleId, pg) {
-    window.location = SUB_SITE+"run_analysis/"+sampleId+"/?page="+pg;
+function downloadFile(sampleId, pg) {
+    window.location = SUB_SITE+"download_file/"+sampleId+"/?page="+pg;
 }
