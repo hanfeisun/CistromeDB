242a0213f1e1f83d0b78e2077e8b56b454fa33f0
Len Taing <lentaing@jimmy.harvard.edu>
2011-08-23 14:03:40 -0400

    batch_update_dataset_form.js: adding tissuetype as a field-- create new tts
    1. modifying the js so that users can create new tts

--- datacollection/views.py
+++ datacollection/views.py
@@ -211,6 +211,7 @@ def form_view_factory(title_in, form_class):
 
 #Cool! but we need the decorators!
 generic_forms_list = ['platform','factor','celltype','cellline', 'cellpop',
+                      'tissuetype',
                       'strain', 'journal', 'species', 'filetype',
                       'assembly', "diseasestate", "control"]
 #new_platform_form = form_view_factory('Platform Form', forms.PlatformForm)
