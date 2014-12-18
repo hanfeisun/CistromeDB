6cbdaa7a9d1eaa094ac46dc55d78a56a7e0c35a4
Hanfei Sun <hfsun.tju@gmail.com>
2014-12-18 08:00:21 -0500

    clean unused files

--- datacollection/email.py
+++
@@ -1,9 +0,0 @@
-"""
-SYNOPSIS: this module is a simple emailer for newdc
-"""
-
-from django.core.mail import send_mail
-
-def newdc_email(subject, msg, recipients_list):
-    send_mail(subject, msg, "cistrome_dc@jimmy.harvard.edu",
-              recipients_list)
